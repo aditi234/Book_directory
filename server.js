4c1eaedcb44db3fe667fe55a37c5f238037f9a6f
Aditi Sahu <aditisahu234@gmail.com>
2021-07-04 20:58:55 +0530

    Finished Author Routes

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose');
 const app = express();
 const expressLayouts = require('express-ejs-layouts');
 const dotenv = require('dotenv');
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
 
 const indexRouter = require('./routes/index');
 const authorRouter = require('./routes/authorRoutes');
@@ -14,6 +15,7 @@ app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
 app.set('layout', 'layouts/layout');
 app.use(expressLayouts);
+app.use(methodOverride('_method'));
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({limit: '10mb', extended: false}))
 
